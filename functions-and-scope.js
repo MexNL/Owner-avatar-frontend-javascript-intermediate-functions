96991fe574b9826d9d0f8f6fa2e5fa0f3055666b
MexNL <mex_kelters@hotmail.com>
2025-04-22 23:17:57 +0200

    eerste push, deel van functions and scope af

--- functions-and-scope.js
+++ functions-and-scope.js
@@ -15,7 +15,14 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // Log het antwoord in de terminal.
 
 // ---- Verwachte uitkomst: 6
-
+// let hoeveelDiplomas = 0;
+//
+// for (let i = 0; i < grades.length; i++) {
+//     if (grades[i] >= 8) {
+//         hoeveelDiplomas++;
+//     }
+// }
+// console.log(hoeveelDiplomas);
 
 /*  1b: Omschrijven tot een herbruikbare functie   */
 // Schrijf een functie genaamd cumLaude, die een array van cijfers verwacht (zoals grades) en het aantal Cum laude studenten teruggeeft. Gebruik hiervoor jouw antwoord van 1a.
@@ -27,8 +34,18 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 // cumLaude([6, 4, 5]) geeft 0
 // cumLaude([8, 9, 4, 6, 10]) geeft 3
 
-
-
+// function cumLaude(grade) {
+//     let hoeveelDiplomas = 0;
+//     for (let i = 0; i < grade.length; i++) {
+//         if (grade[i] >= 8) {
+//             hoeveelDiplomas++;
+//         }
+//     }
+//     return hoeveelDiplomas;
+//
+// }
+//
+// console.log(cumLaude([8, 9, 4, 6, 10]));
 
 /* Opdracht  2: Gemiddeld cijfer */
 
@@ -57,8 +74,6 @@ const grades = [9, 8, 5, 7, 7, 4, 9, 8, 8, 3, 6, 8, 5, 6];
 /* 2c: Afronden op twee decimalen */
 // Zorg ervoor dat het gemiddelde cijfer dat wordt teruggegeven uit de functie netjes wordt afgerond op twee decimalen.
 // Tip: Google is your best friend!
-
-
 
 
 /* Bonusopdracht: hoogste cijfer */
